c4c69d2057ab183ec791a4300691b4bc7840d91a
aleffstampini <aleffkelvin16@gmail.com>
2023-08-19 12:28:39 -0300

    style: code format with prettier+

--- src/CustomRoutes.tsx
+++ src/CustomRoutes.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Routes ,Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home/Home";
 
 const CustomRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route  path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default CustomRoutes;

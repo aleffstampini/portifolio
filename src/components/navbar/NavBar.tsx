27c2d93e07f5d63881b0efbd774e562aba7a381d
aleffstampini <aleffkelvin16@gmail.com>
2023-08-22 05:31:54 -0300

    feat: add smooth scroll and navbar fixed

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
 
 import logo from "../../assets/logo.png";
 
@@ -7,36 +7,38 @@ import "./navbar.style.css";
 const NavBar = () => {
   return (
     <>
-      <nav className="navbar container">
-        <img src={logo} className="logo" alt="Logo" />
-        <ul>
-          <li>
-            <HashLink smooth to="/#init" className="hashlink">
-              Início
-            </HashLink>
-          </li>
-          <li>
-            <HashLink smooth to="/#aboutme" className="hashlink">
-              Sobre Mim
-            </HashLink>
-          </li>
-          <li>
-            <HashLink smooth to="/#technologies" className="hashlink">
-              Tecnologias
-            </HashLink>
-          </li>
-          <li>
-            <HashLink smooth to="/#projects" className="hashlink">
-              Projetos
-            </HashLink>
-          </li>
-          <li>
-            <HashLink smooth to="/#contacts" className="hashlink">
-              Contatos
-            </HashLink>
-          </li>
-        </ul>
-      </nav>
+      <header className="navbar-main">
+        <nav className="navbar container">
+          <img src={logo} className="logo" alt="Logo" />
+          <ul>
+            <li>
+              <HashLink smooth to="/#init" className="hashlink">
+                Início
+              </HashLink>
+            </li>
+            <li>
+              <HashLink smooth to="/#aboutme" className="hashlink">
+                Sobre Mim
+              </HashLink>
+            </li>
+            <li>
+              <HashLink smooth to="/#technologies" className="hashlink">
+                Tecnologias
+              </HashLink>
+            </li>
+            <li>
+              <HashLink smooth to="/#projects" className="hashlink">
+                Projetos
+              </HashLink>
+            </li>
+            <li>
+              <HashLink smooth to="/#contacts" className="hashlink">
+                Contatos
+              </HashLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 };
